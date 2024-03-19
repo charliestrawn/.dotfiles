a6eb12bf47be15b496752976cca38a93a8e62836
Charlie Strawn <charlie.strawn@workiva.com>
2024-03-19 12:35:10 -0500

    move alacritty config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-stow nvim
+stow --target ~/.config nvim
+
+mkdir -p ~/.config/alacritty
 stow --target ~/.config/tmux tmux
+
 stow --target ~/.config spaceship
+
+mkdir -p ~/.config/alacritty
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+stow --target ~/.config/alacritty alacritty
