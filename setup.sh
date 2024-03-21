b21bd8da96f0fee4c8b89d3235b471e9728bd517
Charlie Strawn <charlie.strawn@workiva.com>
2024-03-21 12:34:16 -0500

    add shell config, aliases, and functions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+
+if ![ -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+stow zsh
+
 stow nvim
 
 mkdir -p ~/.config/tmux
