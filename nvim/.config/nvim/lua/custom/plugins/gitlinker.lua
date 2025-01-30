4bd438aae07fb2be5b91470a664e773ef9c3ce24
Charlie Strawn <charlie.strawn@workiva.com>
2025-01-30 13:03:59 -0600

    nvim: fix gitlinker, use maintained fork

--- nvim/.config/nvim/lua/custom/plugins/gitlinker.lua
+++ nvim/.config/nvim/lua/custom/plugins/gitlinker.lua
@@ -1,7 +1,9 @@
 return {
-	"ruifm/gitlinker.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
+	"linrongbin16/gitlinker.nvim",
+	cmd = "GitLink",
+	opts = {},
+	keys = {
+		{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+		{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
 	},
 }
