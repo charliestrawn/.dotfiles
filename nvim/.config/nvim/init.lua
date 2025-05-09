00776e018ab44de0d0fc37e27945cce984a5af76
Charlie Strawn <cdstrawn@gmail.com>
2025-05-09 13:57:53 -0500

    comment transparency, bump lock

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -644,7 +644,7 @@ require("lazy").setup({
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
 			require("tokyonight").setup({
-				transparent = true,
+				-- transparent = true,
 				styles = {
 					comments = { italic = false }, -- Disable italics in comments
 				},
