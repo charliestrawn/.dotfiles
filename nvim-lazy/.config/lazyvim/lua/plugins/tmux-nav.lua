79eea551a6449366ccdb1a950279ce666f021dfb
Charlie Strawn <cdstrawn@gmail.com>
2026-03-09 08:21:08 -0500

    use lazyvim style tmux pane nav

---
+++ nvim-lazy/.config/lazyvim/lua/plugins/tmux-nav.lua
@@ -0,0 +1,13 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true,
+    })
+    local nav = require("nvim-tmux-navigation")
+    vim.keymap.set("n", "<C-h>", nav.NvimTmuxNavigateLeft)
+    vim.keymap.set("n", "<C-j>", nav.NvimTmuxNavigateDown)
+    vim.keymap.set("n", "<C-k>", nav.NvimTmuxNavigateUp)
+    vim.keymap.set("n", "<C-l>", nav.NvimTmuxNavigateRight)
+  end,
+}
