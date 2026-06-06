f337723993379ab64da00c5a6efd616ef7a3beaa
Charlie Strawn <charlie.strawn@workiva.com>
2026-06-06 06:12:22 -0500

    messing with java plugin

--- nvim-lazy/.config/lazyvim/lua/plugins/java.lua
+++ nvim-lazy/.config/lazyvim/lua/plugins/java.lua
@@ -1,16 +1,31 @@
+local home = os.getenv("HOMEj")
+
 return {
   {
     "mfussenegger/nvim-jdtls",
-    opts = function()
-      local jdtls = require("jdtls.setup")
-
-      return {
-        root_dir = jdtls.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-        cmd = { vim.fn.exepath("jdtls") },
-        full_cmd = function(opts)
-          return opts.cmd
-        end,
-      }
-    end,
+    opts = {
+      settings = {
+        java = {
+          format = {
+            enabled = true,
+            settings = {
+              -- Optional: point to your google-java-format or checkstyle XML
+              url = home .. "rrpl-eclipse-style.xml",
+            },
+          },
+        },
+      },
+    },
+    --   opts = function()
+    --     local jdtls = require("jdtls.setup")
+    --
+    --     return {
+    --       root_dir = jdtls.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+    --       cmd = { vim.fn.exepath("jdtls") },
+    --       full_cmd = function(opts)
+    --         return opts.cmd
+    --       end,
+    --     }
+    --   end,
   },
 }
